e9f7e26c3fa93ae43f7addc3fa1b8f2af9e65cbe
DeemaJS <deeema@outlook.com>
2016-02-15 17:57:44 +0200

    Add script task

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var myth = require('gulp-myth');
+var uglify = require('gulp-uglify');
+var jshint = require('gulp-jshint');
 
 // styles task
-
 gulp.task('styles', function() {
 	return gulp.src('app/css/*.css')
 		.pipe(concat('all.css'))
@@ -11,3 +12,15 @@ gulp.task('styles', function() {
 		.pipe(gulp.dest('dist'));
 
 });
+
+// scripts task
+gulp.task('scripts', function() {
+	return gulp.src('app/js/*.css')
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'))
+		.pipe(concat('all.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist'));
+
+});
+
